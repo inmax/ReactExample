2383764a0c78802a00088b3123bf468359e2c2a6
inma <inmaculadaminarroruiz@gmail.com>
2018-09-09 01:33:12 +0200

    integrar slide

--- src/Page/Page3/index.js
+++ src/Page/Page3/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import IntroTest from './components/IntroTest';
+import Swiper from "react-id-swiper";
 
 class Page extends React.Component {
     constructor(props) {
@@ -26,35 +27,89 @@ class Page extends React.Component {
 			showTest : false,
 			showIntro: false,
 			showResult: true,
-		});
+        });
+
 	}
 	againTest = () => {
 		//Condición de cuantas veces se puede repetir el test
-		//resetear todos los campos
+        //resetear todos los campos
+        this.updateSwipe;
 		this.setState({
 			showTest : true,
 			showIntro: false,
 			showResult: false,
 		});
-	}
+    }
+    updateSwiper = () => {
+        if (this.swiper) this.swiper.update();
+    }
+    goNext = () => {
+		if (this.swiper) this.swiper.slideNext();
+	};
+
+	goPrev = () => {
+		if (this.swiper) this.swiper.slidePrev();
+    };
+    
     
     render(){
+
+
+        
+		//https://www.npmjs.com/package/react-id-swiper
+        const params = {
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'fraction',
+            },
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            }
+          };
+
         console.log(this);
         return(
             <div className="test">
-                {/* <div className={"intro step " + (this.state.showIntro === true ? " show " : "")}>
+                <div className={"intro step " + (this.state.showIntro === true ? " show " : "")}>
                     <p>INTRODUCIÓN</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipiscing elit fusce enim mauris consequat porta justo nam eros, magnis donec ridiculus fringilla platea cum dui nisl lobortis ad curabitur id viverra. Eleifend turpis proin potenti nascetur vehicula vel a vestibulum duis, per est felis semper blandit enim tincidunt. Commodo mauris molestie dui congue dignissim diam inceptos dis conubia ac, platea imperdiet cras lobortis fames mi tellus volutpat montes habitant, hac curabitur vehicula malesuada himenaeos scelerisque arcu nostra fringilla.
 
 Sapien facilisis eleifend platea nascetur curabitur eros, dapibus primis vulputate etiam felis ultrices, maecenas molestie feugiat montes eu. Accumsan et curabitur taciti condimentum pulvinar varius lectus magnis rhoncus, magna ultrices hendrerit rutrum placerat urna blandit suscipit, luctus facilisi viverra turpis commodo auctor habitant aliquam. Massa maecenas potenti ultricies rutrum feugiat inceptos eget ac est, platea gravida quam per luctus dictumst nibh ante neque, magna condimentum pulvinar urna tincidunt commodo metus montes.</p>
                     <button onClick={this.startTest} >Empezar</button>
-                </div> */}
+                </div>
 
                 <div className={"preguntas step" + (this.state.showTest === true ? " show " : "")}>
                     SLIDES DE PREGUNTAS
-                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit fusce enim mauris consequat porta justo nam eros, magnis donec ridiculus fringilla platea cum dui nisl lobortis ad curabitur id viverra. Eleifend turpis proin potenti nascetur vehicula vel a vestibulum duis, per est felis semper blandit enim tincidunt. Commodo mauris molestie dui congue dignissim diam inceptos dis conubia ac, platea imperdiet cras lobortis fames mi tellus volutpat montes habitant, hac curabitur vehicula malesuada himenaeos scelerisque arcu nostra fringilla.
+                    <Swiper {...params} ref={(node) => (node ? (this.swiper = node.swiper) : "")}>
+                        <div><div className="item" onClick="this.goNext">
+                            Slide 1
+                            <div className="">
+                                <label className="container-radio">
+                                    Texto de la respuesta
+                                    <input name="campo" type="radio" />
+                                    <span className="checkmark" />
+                                </label>
+                            </div>
+                            <div className="answers__item" onClick="this.goNext">
+                                <div className="">
+                                <label className="container-radio">
+                                    Texto de la respuesta
+                                    <input  name="campo" type="radio" />
+                                    <span className="checkmark" />
+                                </label>
+                            </div>
+                            </div>
 
-Sapien facilisis eleifend platea nascetur curabitur eros, dapibus primis vulputate etiam felis ultrices, maecenas molestie feugiat montes eu. Accumsan et curabitur taciti condimentum pulvinar varius lectus magnis rhoncus, magna ultrices hendrerit rutrum placerat urna blandit suscipit, luctus facilisi viverra turpis commodo auctor habitant aliquam. Massa maecenas potenti ultricies rutrum feugiat inceptos eget ac est, platea gravida quam per luctus dictumst nibh ante neque, magna condimentum pulvinar urna tincidunt commodo metus montes.</p>
+                            </div></div>
+                        <div><div className="item">Slide 2</div></div>
+                        <div><div className="item">Slide 3</div></div>
+                        <div><div className="item">Slide 4</div></div>
+                        <div><div className="item">Slide 5</div></div>
+                    </Swiper>
+                    <button onClick={this.goNext}>Next</button>
+                    <button onClick={this.goPrev}>Prev</button>
+                    
                     <button onClick={this.finishTest} >Resultado</button>
                 </div>
 
